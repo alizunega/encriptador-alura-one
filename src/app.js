faf2751008b45b00084dafc9145481d6311d3ad5
alizunega <alizunega84@gmail.com>
2024-07-11 19:22:42 -0300

    funcionalidad de encriptado

--- src/app.js
+++ src/app.js
@@ -1,9 +1,51 @@
-function adjustHeight(el) {
-  el.style.height = "auto";
-  el.style.height = el.scrollHeight + "px";
+/* VALIDACIONES */
+function validar(inputText) {
+  //expresion regular para minusculas y espacios
+  const validos = /^[a-z\s]*$/;
+  if (!validos.test(inputText)) {
+    alert("Solo se permiten letras minusculas y espacios");
+    return "";
+  }
+  return encriptar(inputText);
 }
+/* VALIDACIONES */
+/* ENCRIPTACION */
+function encriptar(inputText) {
+  const letras = ["a", "e", "i", "o", "u"];
+  let encript = "";
 
-const inputField = document.getElementById("inputField");
-inputField.addEventListener("input", function () {
-  adjustHeight(this);
+  for (let i = 0; i < inputText.length; i++) {
+    if (letras.includes(inputText[i])) {
+      switch (inputText[i]) {
+        case "a":
+          encript += "ai";
+          break;
+        case "e":
+          encript += "enter";
+          break;
+        case "i":
+          encript += "imes";
+          break;
+        case "o":
+          encript += "ober";
+          break;
+        case "u":
+          encript += "ufat";
+          break;
+      }
+    } else {
+      encript += inputText[i];
+    }
+  }
+  return encript;
+}
+/* ENCRIPTACION */
+
+document.getElementById("encript").addEventListener("click", () => {
+  let inputText = document.getElementById("toEncript").value;
+  let encriptado = validar(inputText);
+  if (!encriptado) {
+  }
+  document.getElementById("encripted").innerHTML = encriptado;
+  document.getElementById("toEncript").value = "";
 });
